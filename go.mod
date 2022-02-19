e90da5790c9dade66309e6a6b54597c76d7d489c
Petar Nikov <petar.nikov@gmail.com>
2022-02-19 21:12:32 +0200

    Book: add update endpoint.

--- go.mod
+++ go.mod
@@ -17,5 +17,6 @@ require (
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	gopkg.in/validator.v2 v2.0.0-20210331031555-b37d688a7fb0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
   "net/http"
   "github.com/gin-gonic/gin"
+  "gopkg.in/validator.v2"
   "errors"
   )
 
 type book struct {
   ID string `json:"id"`
-  Title string `json:"title"`
-  Author string `json:"author"`
-  Description string `json:"description"`
-  Quantity int `json:"quantity"`
+  Title string `json:"title" validate:"nonzero"`
+  Author string `json:"author" validate:"nonzero"`
+  Description string `json:"description" validate:"nonzero"`
+  Quantity int `json:"quantity" validate:"nonzero"`
 }
 
 var books = []book{
@@ -30,13 +31,13 @@ func getBooks(c *gin.Context) {
   c.IndentedJSON(http.StatusOK, books)
 }
 
-func getBookById(id string) (book, error) {
-  for _, book := range books {
+func getBookById(id string) (*book, error) {
+  for i, book := range books {
     if book.ID == id {
-      return book, nil
+      return &books[i], nil
     }
   }
-  return book{}, errors.New("Book not found")
+  return nil, errors.New("Book not found")
 }
 
 func getBook(c *gin.Context) {
@@ -61,11 +62,65 @@ func addBook(c *gin.Context) {
     return
   }
 
+  if err:= validator.Validate(newBook); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "error": err.Error(),
+    })
+    return
+  }
+
   books = append(books, newBook)
-  
   c.JSON(http.StatusCreated, gin.H{
     "message": "Book added successfully",
     "book": newBook,
+  })
+}
+
+func updateBook(c *gin.Context) {
+  id := c.Param("id")
+
+  var newBook book
+  
+  err := c.BindJSON(&newBook); if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "message": "Invalid request",
+    })
+    return
+  }
+
+  if err:= validator.Validate(newBook); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "error": err.Error(),
+    })
+    return
+  }
+
+  book, err := getBookById(id); if err != nil {
+    c.JSON(http.StatusNotFound, gin.H{
+      "message": "Book not found",
+    })
+    return
+  }
+
+  if(newBook.Title != book.Title && newBook.Title != ""){
+    book.Title = newBook.Title
+  }
+
+  if(newBook.Author != book.Author && newBook.Author != ""){
+    book.Author = newBook.Author
+  }
+
+  if(newBook.Description != book.Description && newBook.Description != ""){
+    book.Description = newBook.Description
+  }
+
+  if(newBook.Quantity != book.Quantity){
+    book.Quantity = newBook.Quantity
+  }
+
+  c.JSON(http.StatusOK, gin.H{
+    "message": "Book updated successfully",
+    "book": &book,
   })
 }
 
@@ -75,7 +130,7 @@ func main () {
   router.GET("/books", getBooks)
   router.GET("/books/:id", getBook)
   router.POST("/books", addBook)
-  // router.PUT("/books/:id", updateBook)
+  router.PUT("/books/:id", updateBook)
   // router.DELETE("/books/:id", deleteBook)
   router.Run(":8080")
 }
